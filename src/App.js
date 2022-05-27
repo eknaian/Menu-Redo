558f74894d2339354d0e7a75324af4fc7b7c3a20
eknaian <eknaian@gmail.com>
2022-05-27 10:16:11 -0400

    added modal and overlay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Cart from "./components/Cart/Cart";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
