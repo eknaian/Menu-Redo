c58e26aadbf2acbde87c77f3ace0250cdc2d139b
eknaian <eknaian@gmail.com>
2022-05-27 08:38:05 -0400

    added Meals components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
