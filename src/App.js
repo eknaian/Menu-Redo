b2d3ffca30480016bcfe40c9fbddcb90b1cfb58b
eknaian <eknaian@gmail.com>
2022-05-27 11:54:13 -0400

    added functionality to the modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
